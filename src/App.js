192ef4c7958628ef8601b5b942e96102851fb9fb
jiayuez <Jiayue.Zheng@metropolia.fi>
2024-09-12 17:31:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import BookCollectionManager from './components/BookCollectionManager';
 import ShoppingCart from './components/ShoppingCart';
+import RecipeManager from './components/RecipeManager';
 
 function App() {
   return (
